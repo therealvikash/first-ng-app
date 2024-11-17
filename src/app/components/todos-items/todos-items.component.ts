62d24122508ba387fb75e77278fda67f9af2de77
Bikash Yadav <bikashjadav333@gmail.com>
2024-11-17 16:52:41 +0530

    Routeing and Servicing commit

---
+++ src/app/components/todos-items/todos-items.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todos-items',
+  standalone: true,
+  imports: [],
+  templateUrl: './todos-items.component.html',
+  styleUrl: './todos-items.component.scss'
+})
+export class TodosItemsComponent {
+
+}
